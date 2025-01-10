7b0e06d512c1422b69a098937031a53c53f72397
kevinjfa <kevin.fa7@outlook.com>
2025-01-10 18:57:51 +0000

    Server class finished

--- src/main/java/es/software/ulpgc/kata6/Server.java
+++ src/main/java/es/software/ulpgc/kata6/Server.java
@@ -1,5 +1,15 @@
 package es.software.ulpgc.kata6;
 
-public class Server {
+import static spark.Spark.*;
 
+public class Server {
+    public static void start() {
+        port(8080);
+        get("/:command", (req, res) -> {
+            String command = req.params("command");
+            String w1 = req.queryParams("w1");
+            String w2 = req.queryParams("w2");
+            return HttpAdapter.adapt(command, w1, w2);
+        })
+    }
 }
